8276fef8b5cf2af97e4fe8ac0b8c5aa6b24c5dfa
Pabluor <lior.backgammon@gmail.com>
2023-12-12 17:24:32 +0100

    navigation

--- App.js
+++ App.js
@@ -1,25 +1,61 @@
 import React from 'react';
-import { StatusBar, StyleSheet } from 'react-native';
+import { StatusBar } from 'react-native';
+import { Ionicons } from '@expo/vector-icons'
+
 import { RestaurantsScreen } from './src/features/restaurants/screens/restaurants.screen';
+import { SafeArea } from './src/components/utility/safe-area.component';
 import { ThemeProvider } from "styled-components/native";
 import { theme } from "./src/infrastructure/theme";
+
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+
+const Tab = createBottomTabNavigator();
 
 const App = () => {
   return (
     <>
       <ThemeProvider theme={theme}>
-        <RestaurantsScreen />
+        <NavigationContainer>
+          <Tab.Navigator
+            initialRouteName='Restaurants'
+            screenOptions={{
+              tabBarActiveTintColor: theme.colors.brand.primary
+            }}
+          >
+            <Tab.Screen
+              name="Restaurants"
+              component={RestaurantsScreen}
+              options={{
+                tabBarIcon: ({ color, size }) => (
+                  <Ionicons name="restaurant" size={size} color={color} />
+                )
+              }}
+            />
+            <Tab.Screen
+              name="Map"
+              component={SafeArea}
+              options={{
+                tabBarIcon: ({ color, size }) => (
+                  <Ionicons name="map" size={size} color={color} />
+                )
+              }}
+            />
+            <Tab.Screen
+              name="Settings"
+              component={SafeArea}
+              options={{
+                tabBarIcon: ({ color, size }) => (
+                  <Ionicons name="settings" size={size} color={color} />
+                )
+              }}
+            />
+          </Tab.Navigator>
+          <StatusBar style="auto" />
+        </NavigationContainer>
       </ThemeProvider>
-      <StatusBar style="auto" />
     </>
   );
 };
-
-const styles = StyleSheet.create({
-  appContainer: {
-    flex: 1,
-    marginTop: StatusBar.currentHeight || 0,
-  },
-});
 
 export default App;
--- src/features/restaurants/components/restaurant-info.component.js
+++ src/features/restaurants/components/restaurant-info.component.js
@@ -1,6 +1,4 @@
 import React from 'react'
-import { Card } from 'react-native-paper';
-import RestaurantRating from '../../components/restaurant-rating.component';
 import { SvgXml } from 'react-native-svg';
 import open from '../../../assets/open';
 import { Text } from '../../../components/typography/text.component';
